d2413f3f66c4b9883bdb1a0778fbfd69359b1331
alexander.kulpanov <alexander.kulpanov@gmail.com>
2012-07-02 14:28:51 +0000

    initial commit

---
+++ dxe-ts-g729-test/src/dxe-ts-g729-test.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : dxe-ts-g729-test.cpp
+// Author      : Kulpanov & comrades
+// Version     :
+// Copyright   : LGPL
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
